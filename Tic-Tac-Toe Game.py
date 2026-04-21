2b0d4b923a7892359743786ff3d97dd19e331963
gavinburns615 <gsb5322@psu.edu>
2026-04-20 20:28:19 -0400

    Check winner method

--- Tic-Tac-Toe Game.py
+++ Tic-Tac-Toe Game.py
@@ -6,7 +6,17 @@ def print_board(board):
         print("-" * 9)
 
 def check_winner(board, player):
-    pass
+    for row in range(3):
+        if board[row][0] == player and board[row][1] == player and board[row][2] == player:
+            return True
+    for col in range(3):
+        if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+            return True
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    return False
 
 def is_draw(board):
     pass
