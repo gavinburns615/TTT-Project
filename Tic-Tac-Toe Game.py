0b4d669aed902e535572edd6dd8b3f7570b64fe1
gavinburns615 <gsb5322@psu.edu>
2026-04-23 11:12:34 -0400

    main method

--- Tic-Tac-Toe Game.py
+++ Tic-Tac-Toe Game.py
@@ -26,10 +26,39 @@ def is_draw(board):
     return True
 
 def main():
-    pass
+    board = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
+    current_player = "X"
+    print("\nGavin Tic-Tac-Toe Game")
+    game_over = False
+    while game_over == False:
+        print_board(board)
+        print(f"Player {current_player}'s turn")
+        valid_input = False
+        while valid_input == False:
+            try:
+                row = int(input("Enter row (0-2): "))
+                col = int(input("Enter column (0-2): "))
+                if row not in range(3) or col not in range(3):
+                    print("Must enter values between 0 and 2.")
+                elif board[row][col] != " ":
+                    print("That cell is already taken, pick another one")
+                else:
+                    valid_input = True
+            except ValueError:
+                print("Please enter a number from 0-2")
+        board[row][col] = current_player
+        if check_winner(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins")
+            game_over = True
+        elif is_draw(board):
+            print_board(board)
+            print("It is a draw")
+            game_over = True
+        else:
+            if current_player == "X":
+                current_player = "O"    
+            else:
+                current_player = "X"
+#main()
 
-
-
-
-# print_board([[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]])
-main()
